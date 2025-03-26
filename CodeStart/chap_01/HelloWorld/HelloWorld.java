ead16e4d9c4a3cab110c6af3982c8874b2b2b5d4
Dr.Saad.L <56449463+dr-saad-la@users.noreply.github.com>
2025-03-26 18:58:56 +0100

    Update HelloJava file

--- CodeStart/chap_01/HelloWorld/HelloWorld.java
+++ CodeStart/chap_01/HelloWorld/HelloWorld.java
@@ -1,3 +1,15 @@
+/*****************************************************************************
+ * PROJECT:    Java-101 Comprehensive Programming Course
+ * MODULE:     Introduction to Java Fundamentals
+ * LESSON:     1.1 - Hello World Implementation
+ *
+ * AUTHOR:     Dr. Saad Laouadi, Ph.D.
+ * CREATED:    March 2025
+ *
+ * Copyright © 2025 Dr. Saad Laouadi. All Rights Reserved.
+ * See LICENSE file for complete terms.
+ *****************************************************************************/
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
